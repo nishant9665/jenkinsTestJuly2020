ae5d76a28cfff0634a42e1b715d9fb7a9e518a4b
Nishant Mukindrao Narwade <nishant.narwade@yash.com>
2020-07-20 14:09:01 +0530

    first commit

---
+++ src/test/java/sample/FirstTest.java
@@ -0,0 +1,23 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	
+	@Test
+	public void addtest() {
+		System.out.println("This is add test1");
+	}
+	
+	@Test
+	public void subtest() {
+		System.out.println("This is sub test2");
+	}
+	
+	@Test
+	public void multest() {
+		System.out.println("This is mul test3");
+	}
+
+}
